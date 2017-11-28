2642400ac2f3415be5537d8f649208292dd660f2
hebustrolle <ismael.hadj13@gmail.com>
2017-11-28 17:15:10 +0100

    get + id

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PizzaServiceProvider } from '../../providers/pizza-service/pizza-service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private pizza: PizzaServiceProvider) {
+   this.pizza.get().then((items: Array<any>) => {
+     console.log(items);
+   });
+   this.pizza.getById(2).then((item: any) => {
+     console.log(item);
+   });
   }
 
 }
---
+++ src/providers/pizza-service/pizza-service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Pizza } from '../../model/pizza';
+/*
+  Generated class for the PizzaServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class PizzaServiceProvider {
+
+
+  private readonly url = "http://10.13.0.248:3000/pizza/";
+
+  constructor(public http: HttpClient) {
+    console.log('Hello PizzaServiceProvider Provider');
+  }
+
+
+  get(){
+    let rt: Array<Pizza> = new Array<Pizza>();
+    return new Promise<Array<Pizza>>(
+      resolve => {
+        this.http.get(this.url).subscribe((data:Array<any>) => {
+
+          for(let i=0; i<data.length; i++){
+            rt.push(new Pizza(data[i]['id'], data[i]['name'], data[i]['desc'], data[i]['picture'], data[i]['price']))
+          }
+            resolve(rt);});
+      });
+
+  }
+
+
+    getById(id :number){
+
+      let rtbyid: Pizza;
+      return new Promise<Pizza>(
+        resolve => {
+          this.http.get(this.url+id).subscribe((data:any) => {
+             rtbyid = new Pizza(data['id'], data['name'], data['desc'], data['picture'], data['price']);
+              resolve(rtbyid);
+            });
+        });
+
+    }
+
+}
